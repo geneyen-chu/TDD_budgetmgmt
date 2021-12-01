80ae0794d634a3ab2b4d67ebacbdf58ea4e41bdb
Gene_Chu <gene_chu@tw-genechu.client.tw.trendnet.org>
2021-12-01 16:37:24 +0800

    add ut

--- budget_service.py
+++ budget_service.py
@@ -1,8 +1,15 @@
 from datetime import date, datetime, timedelta
 import calendar
+
+class BudgetOjbect:
+    date = ""
+    budget = 0
+    def __init__(self, date, budget) -> None:
+        self.date = date
+        self.budget = budget
 class BudgetService:
     def __init__(self) -> None:
-        pass
+        self.budget_list = self.get_all_budget()
 
     def query(self, s_date, e_date):
         if s_date > e_date:
@@ -10,30 +17,38 @@ class BudgetService:
 
         if s_date.month == e_date.month:
             month_days = calendar.monthrange(s_date.year, s_date.month)[1]
-            daily_budget = self.get_all_budget()[s_date.strftime("%Y%m")] / month_days
+            daily_budget = self.get_monthly_budget(s_date) / month_days
             return daily_budget*(e_date.day-s_date.day+1)
 
         total = 0
         while s_date.strftime("%Y%m") <= e_date.strftime("%Y%m"):            
+            print(s_date.strftime("%Y%m"), e_date.strftime("%Y%m"))
             month_days = calendar.monthrange(s_date.year, s_date.month)[1]
-            daily_budget = self.get_all_budget()[s_date.strftime("%Y%m")] / month_days
+            daily_budget = self.get_monthly_budget(s_date) / month_days
             
             if s_date.month < e_date.month:                
                 rest_day = (month_days-s_date.day+1)
             else:
                 rest_day = (e_date.day)
             print("rest_day", rest_day)
+            print("daily_budget", daily_budget)
+
+            print('total', total)
             total += rest_day*daily_budget
             s_date = s_date + timedelta(days=month_days)
         
         return total
+    
+    def get_monthly_budget(self, date):                
+        for item in self.budget_list:
+            if item.date == date.strftime("%Y%m"):
+                return item.budget
+        return 0
 
     def get_all_budget(self):
-        return {"202101":3100,
-                "202102":2800,
-                "202103":6200}
-
+        return [BudgetOjbect("202105", 3100),
+                BudgetOjbect("202106", 0)]        
 
 if __name__ == __name__:
     b = BudgetService()
-    print(b.query(date(2021,1,1), date(2021,3,10)))
+    print(b.query(date(2021, 5, 1), date(2022, 6, 30)))
--- test_budget.py
+++ test_budget.py
@@ -1,15 +1,49 @@
 import unittest
+from unittest import mock
+from datetime import date
+
 from budget_service import BudgetService
+from budget_service import BudgetOjbect
+
 
 class Test_budgetCase(unittest.TestCase):
     def setUp(self):
+        self.budget_service = BudgetService()
         self.args = (3, 2)
 
     def tearDown(self):
         self.args = None
 
-    def test_queryBedget(self):        
-        self.assertEqual(True, True);
+    def test_query_with_opposite_start_end_date_should_return_zero_budget(self):
+        fake_budget_list = [BudgetOjbect("202105", 3100)]
+
+        self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
+        self.result = self.budget_service.get_all_budget.return_value
+
+        test_result = self.budget_service.query(date(2021, 5, 6), date(2022, 5, 5))
+        self.assertEqual(test_result, 0)
+
+    def test_query_with_one_month_should_return_correct_budget(self):
+        fake_budget_list = [{"202105": 3100}]
+
+        self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
+        self.result = self.budget_service.get_all_budget.return_value
+
+        test_result = self.budget_service.query(date(2021, 5, 1), date(2022, 5, 31))
+        self.assertEqual(test_result, 3100)
+
+    def test_query_with_two_month_should_return_correct_budget(self):
+        fake_budget_list = [{"202105": 3100}, {"202106": 0}]
+
+        self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
+        self.result = self.budget_service.get_all_budget.return_value
+
+        test_result = self.budget_service.query(date(2021, 5, 1), date(2022, 6, 30))
+        self.assertEqual(test_result, 3100)
+
+    def test_queryBudget(self):
+        self.assertEqual(True, True)
 
 
-unittest.main(verbosity=2)
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
