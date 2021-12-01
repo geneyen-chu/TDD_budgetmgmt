1e7801d18e4a20a4518cb8e4a4f677652477dedb
Gene_Chu <gene_chu@tw-genechu.client.tw.trendnet.org>
2021-12-01 17:27:58 +0800

    update ut test

--- test_budget.py
+++ test_budget.py
@@ -16,7 +16,7 @@ class Test_budgetCase(unittest.TestCase):
     def tearDown(self):
         self.args = None
 
-    def test_query_with_opposite_start_end_date_should_return_zero_budget(self):
+    def test_query_with_opposite_start_end_date(self):
         fake_budget_list = [BudgetOjbect("202105", 3100)]
 
         self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
@@ -25,7 +25,7 @@ class Test_budgetCase(unittest.TestCase):
         test_result = self.budget_service.query(date(2021, 5, 6), date(2021, 5, 5))
         self.assertEqual(test_result, 0)
 
-    def test_query_with_one_month_should_return_correct_budget(self):
+    def test_query_with_one_month(self):
         fake_budget_list = [BudgetOjbect("202105", 3100)]
 
         self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
@@ -34,7 +34,7 @@ class Test_budgetCase(unittest.TestCase):
         test_result = self.budget_service.query(date(2021, 5, 1), date(2022, 5, 31))
         self.assertEqual(test_result, 3100)
 
-    def test_query_with_two_month_should_return_correct_budget(self):
+    def test_query_with_two_month(self):
         fake_budget_list = [BudgetOjbect("202105", 3100),
                             BudgetOjbect("202106", 0)]        
 
@@ -44,7 +44,7 @@ class Test_budgetCase(unittest.TestCase):
         test_result = self.budget_service.query(date(2021, 5, 1), date(2021, 6, 30))
         self.assertEqual(test_result, 3100)
 
-    def test_query_with_partial_month_should_return_correct_budget(self):
+    def test_query_with_partial_month(self):
         fake_budget_list = [BudgetOjbect("202104", 6000),
                             BudgetOjbect("202105", 3100)] 
         
@@ -53,21 +53,21 @@ class Test_budgetCase(unittest.TestCase):
         test_result = self.budget_service.query(date(2021, 4, 30), date(2021, 5, 2))
         self.assertEqual(test_result, 400)
 
-    def test_query_with_non_existed_month_should_return_zero_budget(self):
+    def test_query_with_non_existed_month(self):
         fake_budget_list = [BudgetOjbect("202104", 6000)]
         self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
         self.result = self.budget_service.get_all_budget.return_value
-        test_result = self.budget_service.query(date(2021, 5, 1), date(2022, 5, 2))
+        test_result = self.budget_service.query(date(2021, 5, 1), date(2021, 5, 2))
         self.assertEqual(test_result, 0)
 
-    def test_query_with_existed_month_with_zero_budget_should_return_zero_budget(self):
+    def test_query_with_existed_month_with_zero_budget(self):
         fake_budget_list = [BudgetOjbect("202104", 0)]
         self.budget_service.get_all_budget = mock.Mock(return_value=fake_budget_list)
         self.result = self.budget_service.get_all_budget.return_value
-        test_result = self.budget_service.query(date(2021, 4, 16), date(2022, 4, 17))
+        test_result = self.budget_service.query(date(2021, 4, 16), date(2021, 4, 17))
         self.assertEqual(test_result, 0)        
 
-    def test_query_with_partial_two_years_should_return_correct_budget(self):
+    def test_query_with_partial_two_years(self):
         fake_budget_list = [BudgetOjbect("202105", 3100),
                             BudgetOjbect("202106", 9000),
                             BudgetOjbect("202107", 0),
